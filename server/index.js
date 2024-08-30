72500da805bfc97552e7946d9efd19ea631956d1
gilfoyleeee <kushalthapa023@gmail.com>
2024-08-30 15:30:48 +0545

    db connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, ()=> {
-    console.log("Server running on the port 3000 !");
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected !");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server running on the port 3000 !");
+});
