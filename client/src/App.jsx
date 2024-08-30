f864ed8ac9ec5453e66b26636002578d00388c83
gilfoyleeee <kushalthapa023@gmail.com>
2024-08-30 14:02:51 +0545

    routes and pages added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import UserSignUp from "./pages/UserSignUp";
+import UserSignIn from "./pages/UserSignIn";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/user_signup" element={<UserSignUp />} />
+        <Route path="/user_signin" element={<UserSignIn />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
